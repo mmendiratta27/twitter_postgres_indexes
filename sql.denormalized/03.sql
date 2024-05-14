19799c32118bae0927f83aa1bf0fdca7aa3a2f70
mmendiratta27 <mmendiratta27@gmail.com>
2024-05-14 12:33:02 -0700

    done

---
+++ sql.denormalized/03.sql
@@ -0,0 +1,9 @@
+SELECT lang, count(*) AS count
+FROM (
+    SELECT DISTINCT data->>'id' AS id_tweets, data->>'lang' AS lang
+    FROM tweets_jsonb
+    WHERE data ->'entities'->'hashtags' @@ '$[*].text == "coronavirus"' OR data->'extended_tweet'->'entities'->'hashtags' @@ '$[*].text == "coronavirus"'
+) b
+GROUP BY lang
+ORDER BY count DESC, lang
+LIMIT 1000;
